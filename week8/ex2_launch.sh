5c2473dbdde2e9704839dc4a29b62685caaaffc8
raze-or-sharp <olomishcak@outlook.com>
2019-10-09 16:37:07 +0000

    lol

--- week8/ex2_launch.sh
+++
@@ -1,5 +0,0 @@
-gcc ex2.c -o ex2
-echo "Compilation done."
-echo "ex2 is executing..."
-./ex2&
-vmstat 1
--- week8/ex3.sh
+++
@@ -1,6 +0,0 @@
-gcc ex2.c -o ex2
-echo "Compilation done."
-echo "Executing ex2..."
-./ex2&
-echo "Running top -d 1"
-top -d 1
